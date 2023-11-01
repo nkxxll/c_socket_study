12c9b1d06b715bd7334c1e7f0686bff175716907
nkirschall <niki.nice1203@gmail.com>
2023-11-01 01:10:29 +0100

    BATMAN

---
+++ client.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+int main(int argc, char *argv[]) {
+  // make socket
+  // bind
+  // listen
+  // accept
+  // echo
+  return EXIT_SUCCESS;
+}
